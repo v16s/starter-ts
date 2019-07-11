9a127554a844fbccabc831c02ce8b0ce3a78838f
v16s <shankar.vignesh@gmail.com>
2019-07-11 20:45:32 +0530

    added react-apollo

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
+import { Query } from 'react-apollo'
+import { gql } from "apollo-boost";
 import './App.css';
+
+const HELLO = gql`
+{
+  hello
+}
+`
 
 const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Query query={HELLO}>
+      {({ loading, data }: any) => {
+        if (loading) return 'Loading...'
+        return <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+              Edit <code>src/App.tsx</code> and save to reload.
+          </p>
+            <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React
+          </a>
+          </header>
+        </div>
+      }}
+    </Query>
   );
 }
 
